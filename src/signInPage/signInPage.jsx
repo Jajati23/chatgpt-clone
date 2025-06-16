95d4bdd31198c97ab1c45d9adbcf002657e7c87d
jajati.mallick <jajati.mallick@gmail.com>
2025-06-16 21:52:29 +0530

    Added New pages

---
+++ src/signInPage/signInPage.jsx
@@ -0,0 +1,11 @@
+import './signInPage.css'
+
+const signInpage = () => {
+  return (
+    <div className='signInpage'>
+      signInpage
+    </div>
+  )
+}
+
+export default signInpage
---
+++ src/signUpPage/SignUpPage.jsx
@@ -0,0 +1,12 @@
+
+import './signUpPage.css'
+
+const SignUppage = () => {
+  return (
+    <div className='signuppage'>
+      Signuppage
+    </div>
+  )
+}
+
+export default SignUppage
